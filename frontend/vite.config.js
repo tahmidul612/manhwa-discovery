24e45f7b5f618b3be29ebd7f54c5a0850c8d1e12
tahmidul612 <me@tahmidul612.com>
2026-02-12 00:23:26 -0500

    config(frontend): allow external host and telebit access

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
   root: '.',
   server: {
     port: 3009,
+    host: '0.0.0.0',
+    allowedHosts: ['tricky-mule-51.telebit.io'],
     proxy: {
       '/api': {
         target: 'http://localhost:8009',
